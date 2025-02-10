0bbc2ed14fdfbac733241b458ca0239463a2bca7
Randi Febriadi <randifebriadi@gmail.com>
2025-02-10 20:06:34 +0700

    selanjutnya belajar template vite

--- belajar-vite/js/script.js
+++ belajar-vite/js/script.js
@@ -1,7 +1,18 @@
-console.info("Hello World");
-
 import { sayHello } from "./say-hello.js";
 import { sayGoodbye } from "../say-goodbye.ts";
+import { v4 } from "uuid";
+import * as person from "../person.json";
+import textFile from "../hello.txt?raw";
+
+console.info("Hello World");
 
 sayHello("Randi");
 sayGoodbye("Hebat");
+
+console.info(v4());
+
+console.log(person);
+console.log(person.name);
+console.log(person.web);
+console.log(person.instagram);
+console.info(textFile);
